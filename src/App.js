e311894997dee2c90b006624e834a2010614734f
vladetamirkovic <mirkovicvladeta2000@gmail.com>
2021-08-11 16:02:52 +0200

    compound components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
+import './App.scss';
+import { Card } from './components/';
+import movies from './data';
 
 function App() {
   return (
-    <div className="App">
-      <h1>APP</h1>
-
-    </div>
+    <main>
+      {movies.map(movie => (
+        <Card classes='mr' key={`${movie.id}`}>
+          <Card.Image src={movie.image} alt={movie.title} />
+          <Card.Body>
+            <Card.Title>{movie.title}</Card.Title>
+            <Card.Text>{movie.desc}</Card.Text>
+            <Card.Button>{movie.ctaText}</Card.Button>
+          </Card.Body>
+        </Card>
+      ))}
+    </main>
   );
 }
 
